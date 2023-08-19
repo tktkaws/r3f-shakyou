da92e6f4b20b0de726d1c3756516fa1db7fd1268
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-08-19 14:09:43 +0900

    second sample

---
+++ src/other.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Second from './Second.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h2>other.html</h2>
+    <Second />
+  </React.StrictMode>,
+)
